6c20f37db97dbc8135493ce2d25eaf95830a0288
Adxell <adxell02@gmail.com>
2025-07-17 22:00:32 -0500

    feat: Added config to save embedding into database

--- rag_loader/rag_process.py
+++ rag_loader/rag_process.py
@@ -3,6 +3,7 @@ from langchain_community.vectorstores.pgvector import PGVector
 
 from langchain.embeddings.google_palm import GooglePalmEmbeddings
 from config import settings
+from langchain_experimental.text_splitter import SemanticChunker
 
 loader = DirectoryLoader(
     path="../pdfs",
@@ -15,4 +16,18 @@ loader = DirectoryLoader(
 
 docs = loader.load()
 
-embeddings = GooglePalmEmbeddings(google_api_key=settings.api_key_google_palm)
+embeddings = GooglePalmEmbeddings(google_api_key=settings.API_KEY_GOOGLE_PALM)
+
+text_splitter = SemanticChunker(
+    embeddings=embeddings,
+)
+flattend_docs =  [doc[0] for doc in docs if doc]
+flattend_docs = text_splitter.split_documents(flattend_docs)
+
+PGVector.from_documents(
+    documents=flattend_docs,
+    embeddings=embeddings,
+    collection_name="pdfs_collection",
+    connection_string=settings.PGVECTOR_CONNECTION_STRING,
+    pre_delete_collection=True
+)   
